cb32dbccbe77043ea7807f7b4ba8a0465537ecb7
Andy Jiang <andyjiang@gmail.com>
2016-01-12 23:46:34 -0800

    added creator to message

--- server/lib/slack.js
+++ server/lib/slack.js
@@ -59,6 +59,7 @@ function fmtSlack(msg) {
 function print(channel) {
   var str = '\n';
   str += fmt('<#%s>', channel.id) + '\n';
+  str += fmt('  Created by <@%s>', channel.creator) + '\n';
   if (channel.purpose.value) str += fmt('  Purpose: %s\n', channel.purpose.value);
   if (channel.topic.value) str += fmt('  Topic: %s\n', channel.topic.value);
   str += '\n';
